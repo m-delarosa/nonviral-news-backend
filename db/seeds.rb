85835e92824694515aeb21382e995eec7f122e23
Michael De La Rosa <56710845+m-delarosa@users.noreply.github.com>
2020-03-24 14:15:26 -0600

    Add article model, controller, routes, example seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.destroy_all
+
+Article.create(
+    source: "Google",
+    source_url: "www.google.com",
+    url_image: "https://www.androidcentral.com/sites/androidcentral.com/files/styles/large/public/article_images/2020/02/galaxy-s20-ultra-multitasking.jpg?itok=-wSx3YJh",
+    author: "Edgar Allen Poe",
+    title: "Do you keep apps open in the background or close them?",
+    description: "Let's have a chat with the AC forums. In the Android space, there are some questions that will be debated until the end of time. Should you charge your phone overnight? Is wired or wireless charging better? Do you keep your phone on silent or have the ringer …",
+    published_date: "2020-03-17T12:38:53Z"
+    )
