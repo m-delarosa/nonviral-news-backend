e5cf3d14fb8551fa080c6308144b41c9b1b0c1dd
Michael De La Rosa <56710845+m-delarosa@users.noreply.github.com>
2020-03-30 10:07:05 -0600

    Fix article validation bug

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,29 +8,34 @@ class ArticlesController < ApplicationController
         two_days_ago = (Date.today - 2).iso8601
 
         response = RestClient.get(
-        "http://newsapi.org/v2/everything?q=from=#{Date.today}&to=#{two_days_ago}&language=en&pageSize=100&apiKey=33d9acb0b508450cb16344a38197e3e7")
+        "http://newsapi.org/v2/everything?apiKey=33d9acb0b508450cb16344a38197e3e7&q=from=#{Date.today.iso8601}&to=#{two_days_ago}&language=en&pageSize=100")
         
         result = JSON.parse(response)
 
-        articles = result['articles'].select do |article| 
+        valid_articles = result['articles'].select {|article| article['title'] && article['description'] }
+
+        coronafree_articles = valid_articles.select do |article|
+            
+            #Desciption Tests
             corona_test = article['description'].downcase.include? "corona"
             coronavirus_test = article['description'].downcase.include? "coronavirus"
             covid_test = article['description'].downcase.include? "covid-19"
     
+            #Title Tests
             corona_test_title = article['title'].downcase.include? "corona"
             coronavirus_test_title = article['title'].downcase.include? "coronavirus"
             covid_test_title = article['title'].downcase.include? "covid-19"
             pandemic_test = article['title'].downcase.include? "pandemic"
             quarantine_test = article['title'].downcase.include? "quarantine"
-            espanol_test = article['source']['name'].include? "Elespanol.com"
             quarantining_test = article['title'].downcase.include? "quarantining"
 
-            
-    
+            espanol_test = article['source']['name'].include? "Elespanol.com"
+
+
             !corona_test && !coronavirus_test && !covid_test && !corona_test_title && !coronavirus_test_title && !covid_test_title && article['urlToImage'] && !pandemic_test && !quarantine_test && !espanol_test && !quarantining_test
         end
 
-        formatted_articles = articles.map do |article|
+        formatted_articles = coronafree_articles.map do |article|
             {source: article['source']['name'],
             source_url: article['url'],
             url_image: article['urlToImage'],
@@ -41,19 +46,7 @@ class ArticlesController < ApplicationController
         end
 
         render json: formatted_articles
-        # byebug
-        # Tests
-        # def test 
-        # corona_test = article['description'].downcase.include? "corona"
-        # coronavirus_test = article['description'].downcase.include? "coronavirus"
-        # covid_test = article['description'].downcase.include? "covid-19"
 
-        # corona_test_title = article['title'].downcase.include? "corona"
-        # coronavirus_test_title = article['title'].downcase.include? "coronavirus"
-        # covid_test_title = article['title'].downcase.include? "covid-19"
-        # end
-
-        # result['articles'].select do { |article| test }
     end
 
 
