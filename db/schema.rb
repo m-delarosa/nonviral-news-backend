f2335f776105d3fb4e6aa363c7bf03e340e82bf2
Michael De La Rosa <56710845+m-delarosa@users.noreply.github.com>
2020-04-13 12:36:22 -0600

    Fix Fatal Error w/ Rails DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_24_200147) do
+ActiveRecord::Schema.define(version: 0) do
 
-  create_table "articles", force: :cascade do |t|
-    t.string "source"
-    t.text "source_url"
-    t.text "url_image"
-    t.string "author"
-    t.string "title"
-    t.text "description"
-    t.string "published_date"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
 end
